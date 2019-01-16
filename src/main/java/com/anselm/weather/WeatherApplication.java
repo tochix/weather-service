75ed31fd53a050dc0c98ed4f5252d9add7725095
anselm <anselm.emma@gmail.com>
2019-01-16 03:05:29 +0200

    enable task scheduling

--- src/main/java/com/anselm/weather/WeatherApplication.java
+++ src/main/java/com/anselm/weather/WeatherApplication.java
@@ -2,13 +2,14 @@ package com.anselm.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WeatherApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
-
 }
 
